f28a87bf7bf31c9e16e02f2e2b9d173c3e0e0f53
ejr25 <emma.radichkova@gmail.com>
2022-10-14 11:55:16 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Lab 4, Steps 3-4.
+// Pseudocoding a computer task: Computer picks a random 5 letter word, and the user tries to guess it in less than 9 turns.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Emma Radichkova and Maddie Sutherland.
+// @since  10.12.2022
+
+//User inputs a letter
+//Computer denies or accepts letter
+  //Removes letter from list OR fills it in blank
+//Tests win/lose condition
+  //Win condition: Player guesses word before 9th letter
+  //Lose condition: Player takes over 9 turns to get word
+//Repeat step 1-3
