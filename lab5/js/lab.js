64bd04818907f770d4a5bdc789219a5cfe8e7fdf
ejr25 <emma.radichkova@gmail.com>
2022-10-17 21:40:36 -0700

    lab5
    Here is all my completed work for lab5.

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Emma Radihckova eradichk@ucsc.edu
+ * Created: 17 October 2022
+ * License: Public Domain
+*/
+
+// Define Variables
+var model = "Honda";
+var make = "Accord";
+var color = "Red";
+var year = 2007;
+var ownIt = true;
+
+
+// calculate
+var age = 2022 - year;
+
+
+// output
+document.writeln("Model: " + model + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
