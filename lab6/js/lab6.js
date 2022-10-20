356dc17505dddf38f336c2f837e0e9c576b502fa
ejr25 <emma.radichkova@gmail.com>
2022-10-20 07:58:32 -0700

    lab6 v1

---
+++ lab6/js/lab6.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Emma Radihckova eradichk@ucsc.edu
+ * Created: 18 October 2022
+ * License: Public Domain
+*/
+
+
+// Declaring Variables
+var myTransport = ["Santa Cruz Metro", "Campus Loop", "Walking"];
+
+var myMainRide = {
+    make: "Honda",
+    model: "Accord",
+    color: "Red",
+    year: 2007,
+    ownIt: true,
+};
+
+
+// Output
+document.writeln("My forms of transportation: " + myTransport + "<br>");
+document.writeln("My main ride: <pre>" + myMainRide + "<br>");
+  JSON.stringify(myMainRide, null, "\t"), "<pre>");
